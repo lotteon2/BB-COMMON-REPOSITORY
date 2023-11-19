0be2bbc17860654bc342ccb51ac3e761d0c2c14a
nowgnas <dev.nowgnas@gmail.com>
2023-11-19 16:38:03 +0900

    :bookmark: First release

---
+++ src/main/java/bloomingblooms/response/ErrorResponse.java
@@ -0,0 +1,14 @@
+package bloomingblooms.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+  private String code;
+  private String message;
+}
