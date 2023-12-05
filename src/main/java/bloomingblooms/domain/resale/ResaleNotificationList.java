951d5af9895a68eb767fa6163d5f67271e948867
nowgnas <dev.nowgnas@gmail.com>
2023-12-05 20:18:43 +0900

    :bookmark: Add data object

--- src/main/java/bloomingblooms/domain/resale/ResaleNotificationList.java
+++ src/main/java/bloomingblooms/domain/resale/ResaleNotificationList.java
@@ -13,4 +13,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ResaleNotificationList {
   private List<ResaleNotificationData> resaleNotificationData;
+  private String message;
 }
