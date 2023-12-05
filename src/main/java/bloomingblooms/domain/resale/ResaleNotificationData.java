c845c5d865d5f956392264ec7308bb12adb35fb3
nowgnas <dev.nowgnas@gmail.com>
2023-12-05 19:45:43 +0900

    :card_file_box: Add resale notification dto

---
+++ src/main/java/bloomingblooms/domain/resale/ResaleNotificationData.java
@@ -0,0 +1,17 @@
+package bloomingblooms.domain.resale;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ResaleNotification {
+  private Long userId;
+  private String phoneNumber;
+  private String userName;
+}
