b6d4398aca48970140a63b6e3eee092c53764038
qwerty1434 <qwerty1434@naver.com>
2023-12-08 17:04:08 +0900

    :sparkles: Dto 생성

---
+++ src/main/java/bloomingblooms/domain/flower/StockChangeDto.java
@@ -0,0 +1,16 @@
+package bloomingblooms.domain.flower;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockChangeDto {
+    private Long storeId;
+    private Long flowerId;
+    private Long stock;
+}
