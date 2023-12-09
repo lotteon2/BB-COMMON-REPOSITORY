fcf8333568182adbcd2d1bbb1988fa046e5b5f17
nowgnas <dev.nowgnas@gmail.com>
2023-12-10 02:07:09 +0900

    :bookmark: Add notification type

--- src/main/java/bloomingblooms/domain/notification/NotificationKind.java
+++ src/main/java/bloomingblooms/domain/notification/NotificationKind.java
@@ -9,7 +9,8 @@ public enum NotificationKind {
   QUESTION("문의 답변 알림"),
   NEW_ORDER("신규 주문 알림"),
   SETTLEMENT("정산 알림"),
-  LOW_STOCK("재고 부족 알림");
+  LOW_STOCK("재고 부족 알림"),
+  NEWCOMER("신규 회원 가입 알림");
   private final String kind;
 
   NotificationKind(String kind) {
