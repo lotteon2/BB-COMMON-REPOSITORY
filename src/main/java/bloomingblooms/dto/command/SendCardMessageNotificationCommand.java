083639ca2cacbbef6c8e2c1df766dafcc99a60f5
indl1670 <81145399+indl1670@users.noreply.github.com>
2023-12-15 21:17:48 +0900

    💜 fix: fix className

--- src/main/java/bloomingblooms/dto/command/SendCardMessageNotificationCommand.java
+++ src/main/java/bloomingblooms/dto/command/SendCardMessageNotificationCommand.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class ModifyOrderStateCommand {
+public class SendCardMessageNotificationCommand {
     private Long cardId;
     private String password;
     private String senderName;
