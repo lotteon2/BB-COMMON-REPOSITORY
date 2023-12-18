90a909c6e2dad41bd4a756dbc6d9bb0e066bb580
nowgnas <dev.nowgnas@gmail.com>
2023-12-18 12:04:08 +0900

    :bookmark: Amend notification dto

--- src/main/java/bloomingblooms/domain/notification/PublishNotificationInformation.java
+++ src/main/java/bloomingblooms/domain/notification/PublishNotificationInformation.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class PublishNotificationInformation {
   private NotificationKind notificationKind;
   private String notificationUrl;
+  private String content;
   private Role role;
 
   public static PublishNotificationInformation updateRole(
@@ -20,7 +21,17 @@ public class PublishNotificationInformation {
     return PublishNotificationInformation.builder()
         .notificationKind(publishData.getNotificationKind())
         .notificationUrl(publishData.notificationUrl)
+        .content(publishData.getContent())
         .role(role)
+        .build();
+  }
+
+  public static PublishNotificationInformation getData(
+      NotificationURL url, NotificationKind kind, String content) {
+    return PublishNotificationInformation.builder()
+        .notificationKind(kind)
+        .notificationUrl(url.getUrl())
+        .content(content)
         .build();
   }
 
