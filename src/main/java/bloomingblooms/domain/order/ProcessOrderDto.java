3b325706832a9e6f84264eadd56517064dfb1c7f
binarywoo27 <blu_mountain@naver.com>
2023-12-20 19:17:19 +0900

    :sparkles: 주문 ProcessOrderDto 추가

---
+++ src/main/java/bloomingblooms/domain/order/ProcessOrderDto.java
@@ -0,0 +1,19 @@
+package bloomingblooms.dto.command;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProcessOrderDto {
+  private String orderId;
+  private String orderType;
+  private List<Long> couponIds;
+  private Map<String, Long> products; // productId: quantity
+  private Long userId;
+  private String phoneNumber;
+}
