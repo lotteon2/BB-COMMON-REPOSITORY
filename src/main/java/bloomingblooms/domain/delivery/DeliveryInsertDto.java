17cfac2acdea518ba929cebae184dfcb726b20a9
binarywoo27 <blu_mountain@naver.com>
2023-12-22 17:45:25 +0900

    :pencil2: 누락된 함수 추가 및 불필요한 Dto 제거

--- src/main/java/bloomingblooms/domain/delivery/DeliveryInsertDto.java
+++ src/main/java/bloomingblooms/domain/delivery/DeliveryInsertDto.java
@@ -1,9 +1,5 @@
 package bloomingblooms.domain.delivery;
 
-import bloomingblooms.domain.order.OrderInfo;
-import bloomingblooms.domain.order.OrderInfoByStore;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,30 +20,4 @@ public class DeliveryInsertDto {
   private String addressDetail;
   private String request;
   private Long deliveryCost;
-
-  public static List<DeliveryInsertDto> toDto(OrderInfo orderInfo) {
-
-    // 가게 개수 만큼 배송 정보 생성
-    List<OrderInfoByStore> orderInfoByStores = orderInfo.getOrderInfoByStores();
-
-    List<DeliveryInsertDto> list = new ArrayList<>();
-    for (int i = 0; i < orderInfoByStores.size(); i++) {
-      DeliveryInsertDto dto =
-          DeliveryInsertDto.builder()
-              .ordererName(orderInfo.getOrdererName())
-              .ordererPhoneNumber(orderInfo.getOrdererPhoneNumber())
-              .ordererEmail(orderInfo.getOrdererEmail())
-              .recipientName(orderInfo.getRecipientName())
-              .recipientPhoneNumber(orderInfo.getRecipientPhone())
-              .zipcode(orderInfo.getDeliveryZipcode())
-              .roadName(orderInfo.getDeliveryRoadName())
-              .addressDetail(orderInfo.getDeliveryAddressDetail())
-              .request(orderInfo.getDeliveryRequest())
-              .deliveryCost(orderInfoByStores.get(i).getDeliveryCost())
-              .build();
-      list.add(dto);
-    }
-
-    return list;
-  }
 }
--- src/main/java/bloomingblooms/domain/payment/KakaopayApproveRequestDto.java
+++ src/main/java/bloomingblooms/domain/payment/KakaopayApproveRequestDto.java
@@ -1,7 +1,5 @@
 package bloomingblooms.domain.payment;
 
-import bloomingblooms.domain.order.OrderInfo;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,19 +21,4 @@ public class KakaopayApproveRequestDto {
   private String cid;
   private String tid;
   private String pgToken;
-
-  public static KakaopayApproveRequestDto toDto(OrderInfo orderInfo, String orderType) {
-    return KakaopayApproveRequestDto.builder()
-        .userId(orderInfo.getUserId())
-        .orderId(orderInfo.getTempOrderId())
-        .orderType(orderType)
-        .itemName(orderInfo.getItemName())
-        .quantity(Math.toIntExact(orderInfo.getSumOfAllQuantity()))
-        .totalAmount(Math.toIntExact(orderInfo.getSumOfActualAmount()))
-        .taxFreeAMount(0)
-        .isSubscriptionPay(orderInfo.isSubscriptionPay())
-        .tid(orderInfo.getTid())
-        .pgToken(orderInfo.getPgToken())
-        .build();
-  }
 }
