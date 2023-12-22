ac1a01d7325c447fba77ff3a6e336ee14c0e86a5
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-12-22 12:40:59 +0900

    add: add the bilder at CartProductItemInfo

--- src/main/java/bloomingblooms/domain/wishlist/cart/CartProductItemInfo.java
+++ src/main/java/bloomingblooms/domain/wishlist/cart/CartProductItemInfo.java
@@ -2,12 +2,14 @@ package bloomingblooms.domain.wishlist.cart;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class CartProductItemInfo {
 
   private Long storeId;
