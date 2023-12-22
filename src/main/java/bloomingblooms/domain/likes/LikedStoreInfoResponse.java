2b78e09bb6131bfb63b9cdede72c6b9f380adb89
qwerty1434 <qwerty1434@naver.com>
2023-12-22 13:26:37 +0900

    :pencil2: builder 추가

--- src/main/java/bloomingblooms/domain/likes/LikedStoreInfoResponse.java
+++ src/main/java/bloomingblooms/domain/likes/LikedStoreInfoResponse.java
@@ -1,11 +1,13 @@
 package bloomingblooms.domain.likes;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Getter
 public class LikedStoreInfoResponse {
 
