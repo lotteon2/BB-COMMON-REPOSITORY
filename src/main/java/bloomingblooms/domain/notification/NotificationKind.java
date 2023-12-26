e66b04531e6da6b2963e8aa030ea5bdb89d06044
nowgnas <dev.nowgnas@gmail.com>
2023-12-26 10:17:41 +0900

    :bookmark: Amend dto

--- src/main/java/bloomingblooms/domain/notification/NotificationKind.java
+++ src/main/java/bloomingblooms/domain/notification/NotificationKind.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum NotificationKind {
   DELIVERY("배송이 시작되었습니다."),
   RESALE("상품이 재입고 되었습니다."),
-  QUESTION("문의에 답변이 등록되었습니다."),
+  QUESTION("문의가 등록되었습니다."),
+  INQUERY("문의에 답변이 등록되었습니다."),
   SHOPPINGMALL("신규 배송 주문이 접수 되었습니다."),
   PICKUP("신규 픽업 주문이 접수 되었습니다."),
   SUBSCRIBE("신규 구독 주문이 접수 되었습니다."),
