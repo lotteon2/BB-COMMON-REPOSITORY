ebd62d60f44471535a5c1da1fb0470bf52c77e18
binarywoo27 <blu_mountain@naver.com>
2023-12-26 20:43:32 +0900

    :sparkles: 신규 배송지 추가 Dto

--- src/main/java/bloomingblooms/domain/delivery/DeliveryAddressInsertDto.java
+++ src/main/java/bloomingblooms/domain/delivery/DeliveryAddressInsertDto.java
@@ -1,2 +1,20 @@
-package bloomingblooms.domain.delivery;public class DeliveryAddressInsertDto {
+package bloomingblooms.domain.delivery;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliveryAddressInsertDto {
+  private Long deliveryAddressId;
+  private Long userId;
+  private String recipientName;
+  private String zipcode;
+  private String roadName;
+  private String addressDetail;
+  private String phoneNumber;
 }
