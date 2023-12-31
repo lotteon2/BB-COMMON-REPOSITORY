89a9fec33c945401e5907a98172ad23047547e5a
binarywoo27 <blu_mountain@naver.com>
2023-12-31 21:11:43 +0900

    :sparkles: 배치 정기결제용 Kafka Dto

---
+++ src/main/java/bloomingblooms/domain/batch/SubscriptionBatchDto.java
@@ -0,0 +1,19 @@
+package bloomingblooms.domain.batch;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionBatchDto {
+  private String cid;
+  private String sid;
+  private String partnerOrderId;
+  private String partnerUserId;
+  private Long quantity;
+  private Long totalAmount;
+}
