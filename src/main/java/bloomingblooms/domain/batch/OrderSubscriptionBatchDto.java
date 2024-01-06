90fd0eb7197cb7a31d4ce8e8abe30894f28ab049
binarywoo27 <blu_mountain@naver.com>
2024-01-06 21:52:31 +0900

    feat: 배치) 정기구독 주문 프로세스 변경에 따른 DTO 추가

---
+++ src/main/java/bloomingblooms/domain/batch/OrderSubscriptionBatchDto.java
@@ -0,0 +1,15 @@
+package bloomingblooms.domain.batch;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderSubscriptionBatchDto {
+  private List<String> orderSubscriptionIds;
+}
