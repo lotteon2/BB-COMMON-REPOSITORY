3c203f38830f4a9b64e03acbac6152416bcd4484
binarywoo27 <blu_mountain@naver.com>
2024-01-11 17:48:44 +0900

    bug: Dto 수정

--- src/main/java/bloomingblooms/domain/delivery/DeliveryInfoDto.java
+++ src/main/java/bloomingblooms/domain/delivery/DeliveryInfoDto.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeliveryInfoDto {
+  private Long deliveryId;
+  private String deliveryTrackingNumber;
   private String ordererName;
   private String ordererPhone;
   private String ordererEmail;
