9994bd65a0f8aeefaef8570bc948658388e2d74c
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-01-12 09:39:30 +0900

    Update SettlementStoreInfoResponse.java

--- src/main/java/bloomingblooms/dto/response/SettlementStoreInfoResponse.java
+++ src/main/java/bloomingblooms/dto/response/SettlementStoreInfoResponse.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 public class SettlementStoreInfoResponse {
-
+  private Long storeId;
   private String storeName;
   private String bankName;
   private String accountNumber;
