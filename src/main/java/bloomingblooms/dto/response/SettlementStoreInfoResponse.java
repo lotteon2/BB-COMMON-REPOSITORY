9b761b82dd159be85fe08e7d872401b16f410c61
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-01-12 09:20:59 +0900

    add : add the SettlementStoreInfoResponse for getting Settlement store info

---
+++ src/main/java/bloomingblooms/dto/response/SettlementStoreInfoResponse.java
@@ -0,0 +1,20 @@
+package bloomingblooms.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class SettlementStoreInfoResponse {
+
+  private String storeName;
+  private String bankName;
+  private String accountNumber;
+  private String gugun;
+  private String sido;
+}
