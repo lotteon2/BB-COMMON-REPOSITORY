ffc0dbff4611ddb59c42c0aeb8b628488f711573
binarywoo27 <blu_mountain@naver.com>
2024-01-15 19:27:45 +0900

    fix: 배송, 구독 상품의 배송상태 변경시 같은 Dto를 사용하도록 수정

--- src/main/java/bloomingblooms/domain/delivery/UpdateOrderStatusDto.java
+++ src/main/java/bloomingblooms/domain/delivery/UpdateOrderStatusDto.java
@@ -1,6 +1,7 @@
 package bloomingblooms.domain.delivery;
 
 import bloomingblooms.domain.notification.delivery.DeliveryStatus;
+import bloomingblooms.domain.notification.order.OrderType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateOrderStatusDto {
-  private String orderDeliveryId;
+  private String orderId;  // orderDeliveryId, orderSubscriptionId
+  private OrderType orderType;
   private String phoneNumber;
   private DeliveryStatus deliveryStatus;
 }
